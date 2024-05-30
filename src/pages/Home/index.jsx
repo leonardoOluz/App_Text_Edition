af497ec105fb4ef804625ffbab9760328e48d6b8
Leonardo Luz <leonardoluz10@hotmail.com>
2024-05-30 10:58:18 -0300

    feat: :white_check_mark: Refatorando arquivo Editor - Home.

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Formulario from "components/Formulario";
+import Secao from "components/Secao";
+
+const Home = () => {
+    return (<main>
+        <Secao>
+            <Formulario />
+        </Secao>
+    </main>)
+}
+
+export default Home;
